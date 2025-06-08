f004c31c31a71cd2dd3e05ebc1e4bfeeb6af30e0
rl627 <rl627@drexel.edu>
2025-06-08 15:49:31 -0400

    refactor: migrated boudsn to src/data

--- src/App.svelte
+++ src/App.svelte
@@ -28,6 +28,10 @@
   import Filler from './layout/Filler.svelte';
   import Divider from './layout/Divider.svelte';
   import Arrow from './ui/Arrow.svelte';
+  
+  // Map configuration
+  import { bounds } from './data/mapBounds.js';
+  
   let theme = 'light';
   setContext('theme', theme);
   setColors(themes, theme);
@@ -49,41 +53,6 @@
   const hex_background = '#d8e6ed';
   const hex_purple = '#8C198C';
   const hex_green = '#305145';
-
-  const bounds = {
-    southAmerica: [
-      [-117.157278, -55.058347], // Southwest coordinates
-      [-66.273339, 34], // Northeast coordinates
-    ],
-    southAmericaShifted: [
-      [-97.157278, -55.058347], // Southwest coordinates (shifted)
-      [-46.273339, 34], // Northeast coordinates (shifted)
-    ],
-    municipio: [
-      [-46.826191, -24.008374],
-      [-46.365357, -23.357529],
-    ],
-    metro: [
-      [-47.2054645, -24.0642507],
-      [-45.6952955, -23.184244],
-    ],
-    l1ad: [
-      [-47.084742, -24.008374],
-      [-46.051954, -23.183419],
-    ],
-    ex_l2: [
-      [-46.584936, -23.650164],
-      [-46.544989, -23.600327],
-    ],
-    monterrey: [
-      [-100.8619014, 25.2237859], // Southwest coordinates
-      [-99.6864261, 26.3951673], // Northeast coordinates
-    ],
-    rio_cuarto: [
-      [-65.1437809, -34.2456892], // Southwest coordinates
-      [-63.8864557, -32.3048849], // Northeast coordinates
-    ],
-  };
 
   // Data
 
---
+++ src/data/mapBounds.js
@@ -0,0 +1,34 @@
+export const bounds = {
+  southAmerica: [
+    [-117.157278, -55.058347], // Southwest coordinates
+    [-66.273339, 34], // Northeast coordinates
+  ],
+  southAmericaShifted: [
+    [-97.157278, -55.058347], // Southwest coordinates (shifted)
+    [-46.273339, 34], // Northeast coordinates (shifted)
+  ],
+  municipio: [
+    [-46.826191, -24.008374],
+    [-46.365357, -23.357529],
+  ],
+  metro: [
+    [-47.2054645, -24.0642507],
+    [-45.6952955, -23.184244],
+  ],
+  l1ad: [
+    [-47.084742, -24.008374],
+    [-46.051954, -23.183419],
+  ],
+  ex_l2: [
+    [-46.584936, -23.650164],
+    [-46.544989, -23.600327],
+  ],
+  monterrey: [
+    [-100.8619014, 25.2237859], // Southwest coordinates
+    [-99.6864261, 26.3951673], // Northeast coordinates
+  ],
+  rio_cuarto: [
+    [-65.1437809, -34.2456892], // Southwest coordinates
+    [-63.8864557, -32.3048849], // Northeast coordinates
+  ],
+};
