733b487fa44f6490a99850cbc293d1e6aabf0cd9
rl627 <rl627@drexel.edu>
2025-06-08 22:07:10 -0400

    removed uneed dependncies in app.svelte

--- src/components/sections/StepOneSection.svelte
+++ src/components/sections/StepOneSection.svelte
@@ -2,13 +2,29 @@
   import Section from '../../layout/Section.svelte';
   import Media from '../../layout/Media.svelte';
   import { Map, MapSource, MapLayer, MapTooltip } from '../../../libs/@onsvisual/svelte-maps';
+  import { bounds } from '../../data/mapBounds.js';
+  import { getTopo } from '../../utils.js';
 
-  export let bounds;
-  export let geojson_salurbal_centroid;
-  export let map_static_1;
-  export let center;
-  export let hovered;
-  export let hex_warning;
+  // Internal state - no longer accepting props
+  let map_static_1;
+  let center;
+  let hovered;
+  
+  // Color constant
+  const hex_warning = '#BC812F';
+  
+  // Data source for SALURBAL centroids
+  const src__salurbal_centroid = {
+    url: './data/salurbal_l1ad_centroid.json',
+    layer: 'geog',
+    code: 'salid2',
+  };
+  let geojson_salurbal_centroid;
+  getTopo(src__salurbal_centroid.url, src__salurbal_centroid.layer).then(
+    (res) => {
+      geojson_salurbal_centroid = res;
+    }
+  );
 </script>
 
 <Section>
@@ -29,46 +45,46 @@
         > to obtain a list of all cities (as defined in these sources) with 100,000
         residents or more in 2010. We combined both lists and eliminated duplicates.
       </p>
-    </div>
-
-    <div class="right-col">
+    </div>    <div class="right-col">
       <Media col="medium" caption="Interactive map of all 371 SALURBAL cities">
         <div class="chart-sml">
-          <Map
-            id="static_map_1"
-            style="./data/style-osm-grey.json"
-            location={{ bounds: bounds.southAmericaShifted }}
-            controls={true}
-            scales={false}
-            dragPan={true}
-            bind:map={map_static_1}
-            bind:center
-          >
-            <MapSource
-              map_id="static_map_1"
-              id="static_map_1-src"
-              type="geojson"
-              data={geojson_salurbal_centroid}
-              promoteId={'l1_label'}
-              maxzoom={13}
+          {#if geojson_salurbal_centroid}
+            <Map
+              id="static_map_1"
+              style="./data/style-osm-grey.json"
+              location={{ bounds: bounds.southAmericaShifted }}
+              controls={true}
+              scales={false}
+              dragPan={true}
+              bind:map={map_static_1}
+              bind:center
             >
-              <MapLayer
+              <MapSource
                 map_id="static_map_1"
-                id="static_map_1-circle"
-                type="circle"
-                paint={{
-                  'circle-color': hex_warning,
-                  'circle-radius': 2.5,
-                  'circle-stroke-color': 'black',
-                  'circle-stroke-width': 0.1,
-                }}
-                hover={true}
-                bind:hovered
+                id="static_map_1-src"
+                type="geojson"
+                data={geojson_salurbal_centroid}
+                promoteId={'l1_label'}
+                maxzoom={13}
               >
-                <MapTooltip map_id="static_map_1" content={`${hovered}`} />
-              </MapLayer>
-            </MapSource>
-          </Map>
+                <MapLayer
+                  map_id="static_map_1"
+                  id="static_map_1-circle"
+                  type="circle"
+                  paint={{
+                    'circle-color': hex_warning,
+                    'circle-radius': 2.5,
+                    'circle-stroke-color': 'black',
+                    'circle-stroke-width': 0.1,
+                  }}
+                  hover={true}
+                  bind:hovered
+                >
+                  <MapTooltip map_id="static_map_1" content={`${hovered}`} />
+                </MapLayer>
+              </MapSource>
+            </Map>
+          {/if}
         </div>
       </Media>
     </div>
