7160f446b27b161c9ffab71a559a2316251b38db
rl627 <rl627@drexel.edu>
2025-06-10 08:14:14 -0400

    format header doc #8

--- src/layout/Header.svelte
+++ src/layout/Header.svelte
@@ -8,11 +8,10 @@
   export let center = true;
 </script>
 
-<nav
-  class="parent"
-
->
-  <div class="float-container">    <div class="float-child col-wide middle" class:center>      <a href="https://lacurbanhealth.org">
+<nav class="parent">
+  <div class="float-container">
+    <div class="float-child col-wide middle" class:center>
+      <a href="https://lacurbanhealth.org">
         <div id="svg-container">
           <img
             id="my-svg"
@@ -23,7 +22,8 @@
       </a>
     </div>
 
-    <div class="float-child container_container">      <div class="container">
+    <div class="float-child container_container">
+      <div class="container">
         <!-- <a href="https://github.com/Drexel-UHC">
         <div class="social-icon"><Icon icon="mdi:github" /></div>
       </a> -->
@@ -32,15 +32,14 @@
         </a>
         <a href="https://x.com/lacurbanhealth">
           <div class="social-icon"><Icon icon="mdi:twitter" /></div>
-        </a>        <a href="https://www.instagram.com/salurbalclimate/">
+        </a>
+        <a href="https://www.instagram.com/salurbalclimate/">
           <div class="social-icon"><Icon icon="mdi:instagram" /></div>
         </a>
         <a href="https://www.youtube.com/@salurbal">
           <div class="social-icon"><Icon icon="mdi:youtube" /></div>
         </a>
-        <a
-          href="https://www.linkedin.com/company/salurbal-project"
-        >
+        <a href="https://www.linkedin.com/company/salurbal-project">
           <div class="social-icon"><Icon icon="mdi:linkedin" /></div>
         </a>
       </div>
@@ -48,7 +47,8 @@
   </div>
 </nav>
 
-<style>  #svg-container {
+<style>
+  #svg-container {
     width: 50px; /* Increased width for better visibility */
     height: 25px; /* Set height to 25px as requested */
     display: flex;
@@ -60,10 +60,11 @@
     height: 100%; /* Make the SVG height fill the container */
     object-fit: contain; /* Adjust the object-fit property as per your needs */
   }
-  
+
   .parent {
     height: 30px; /* Reset to original height for smaller logo */
-  }  nav {
+  }
+  nav {
     -webkit-box-sizing: border-box;
     box-sizing: border-box;
     position: fixed; /* Changed from relative to fixed */
@@ -85,7 +86,8 @@
     flex-direction: row;
     align-content: center;
     justify-content: space-between;
-  }  .container {
+  }
+  .container {
     display: flex;
     flex-direction: row;
     align-items: center;
