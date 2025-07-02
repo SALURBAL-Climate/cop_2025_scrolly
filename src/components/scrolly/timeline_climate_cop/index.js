e9de8b0ec315b63bd9d1c62547e9b115d84c1de3
rl627 <rl627@drexel.edu>
2025-07-02 14:27:14 -0400

    delete unused scrolyl component

--- src/components/scrolly/timeline_climate_cop/index.js
+++
@@ -1,3 +0,0 @@
-// Export the main component and data for easy importing
-export { default as TimelineClimateCOP } from './TimelineClimateCop.svelte';
-export { timelineData, timelineMetadata, cop3, cop21} from './data.js';
