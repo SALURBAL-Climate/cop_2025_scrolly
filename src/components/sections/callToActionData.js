b41ec467410f3d89af882afe3639d974d13e16ed
rl627 <rl627@drexel.edu>
2025-07-23 10:47:38 -0400

    sync callToActionData to final englihs pdf content #14

--- src/components/sections/callToActionData.js
+++ src/components/sections/callToActionData.js
@@ -2,31 +2,31 @@ export const actionItems = [
   {
     iconSrc: "./img/cop30/icon_action_pen.png",
     iconAlt: "Research Products Icon",
-    description: "Developing <b>user-friendly research products</b> to support advocacy surrounding the health impacts of climate change and the impacts of mitigation and adaptation action.",
+    description: "<b>Developing user-friendly research products</b> to support advocacy surrounding the health impacts of climate change and the impacts of mitigation and adaptation action.",
     alignment: "left"
   },
   {
     iconSrc: "./img/cop30/icon_action_talk.png",
     iconAlt: "COP Delegates Icon", 
-    description: "Connecting with <b>COP delegates and observers</b> to provide input and expertise throughout COP preparatory sessions.",
+    description: "<b>Connecting with COP delegates and observers</b> to provide input and expertise throughout COP preparatory sessions.",
     alignment: "right"
   },
   {
     iconSrc: "./img/cop30/icon_action_microphone.png",
     iconAlt: "Health Advocacy Icon",
-    description: "Advocating for the inclusion of <b>health impacts and health arguments within NDCs</b> and other climate policy and planning documents.",
+    description: "<b>Advocating for health inclusion</b> for the inclusion of health impacts and health arguments within NDCs and other climate policy and planning documents.",
     alignment: "left"
   },
   {
     iconSrc: "./img/cop30/icon_action_hand.png",
     iconAlt: "Green Zone Activities Icon",
-    description: "Hosting and participating in <b>health-focused green zone activities</b> (pavilions, panels, presentations).",
+    description: "<b>Hosting and participating in health-focused green zone activities</b> (pavilions, panels, presentations).",
     alignment: "right"
   },
   {
     iconSrc: "./img/cop30/icon_action_point_marker.png",
     iconAlt: "Municipal Partnerships Icon",
-    description: "Forming <b>partnerships with municipal governments and urban practitioners</b>. Local governments worldwide are leading climate action through mitigation and adaptation measures. Many are also leveraging the health co-benefits of these efforts. Researchers can support them by supplying and discussing localized data and evidence.",
+    description: "<b>Forming partnerships with municipal governments</b> and urban practitioners. Local governments worldwide are leading climate action through mitigation and adaptation measures. Many are also leveraging the health co-benefits of these efforts. Researchers can support them by supplying and discussing localized data and evidence.",
     alignment: "left"
   }
 ];
