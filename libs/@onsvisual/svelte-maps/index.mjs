62d5a9b6097cc1f4544ea37d47a2f903f6c3577b
rl627 <rl627@drexel.edu>
2025-07-23 13:47:49 -0400

    removed libs folder (unused legacy svlete-map/chart) code

--- libs/@onsvisual/svelte-maps/index.mjs
+++
@@ -1,7 +0,0 @@
-// MAP COMPONENTS
-export { default as Map } from './src/Map.svelte';
-export { default as StaticMap1 } from './src/StaticMap1.svelte';
-export { default as MapSource } from './src/MapSource.svelte';
-export { default as MapLayer } from './src/MapLayer.svelte';
-export { default as MapTooltip } from './src/MapTooltip.svelte';
-export { default as MapPopup } from './src/MapPopup.svelte';
